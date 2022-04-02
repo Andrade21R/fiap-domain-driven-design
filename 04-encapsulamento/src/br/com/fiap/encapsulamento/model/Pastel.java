34eed0d19152fbe9bcc5a7290c5313d55868b87e
Rafael Andrade <rafael.santos.andrade@hotmail.com>
2022-04-01 22:57:32 -0300

    01/04/2022

---
+++ 04-encapsulamento/src/br/com/fiap/encapsulamento/model/Pastel.java
@@ -0,0 +1,24 @@
+package br.com.fiap.encapsulamento.model;
+
+public class Pastel {
+
+	private String sabor;
+
+	private boolean pronto;
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public boolean isPronto() {
+		return pronto;
+	}
+
+	public void setPronto(boolean pronto) {
+		this.pronto = pronto;
+	}
+}
